876ef1cb39d8d5b2366b075c61393c391ad7c0fa
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2025-06-07 23:21:37 +0200

    chandle error

--- src/app/(storefront)/checkout/components/stripe.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe, Stripe } from "@stripe/stripe-js";
-
-// export const stripePromise = loadStripe(
-//   process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!
-// );
-
-export const stripePromise: Promise<Stripe | null> = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
-);
